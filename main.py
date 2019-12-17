b6e08d29663c6d7ec9e4b8e9127aaa658ef030c6
Fernando Camaro Nogues <fernando@ascent.ai>
2019-12-17 19:30:38 +0900

    add the pitting against function to decide if the new trained network is accepted as the new model

--- main.py
+++ main.py
@@ -21,7 +21,14 @@ def muzero_training(config: MuZeroConfig):
     # import pickle
     # pickle.dump( replay_buffer, open( "save.pkl", "wb" ) )
     # replay_buffer = pickle.load( open( "save.pkl", "rb" ) )
-    train_network(config, storage, replay_buffer)
+    trained_network = train_network(config, storage, replay_buffer)
+    pwins, nwins, draws = pit_against(config, storage, trained_network)
+    print('NEW/PREV WINS : %d / %d ; DRAWS : %d' % (nwins, pwins, draws))
+    if pwins+nwins == 0 or float(nwins)/(pwins+nwins) < 0.55:
+        print('REJECTING NEW MODEL')
+    else:
+        print('ACCEPTING NEW MODEL')
+        storage.save_network(i, trained_network)
   net = storage.latest_network()
   torch.save({
         'state_dict': net.model.state_dict()
@@ -61,6 +68,39 @@ def play_game(config: MuZeroConfig, network: Network) -> Game:
     game.apply(action)
     game.store_search_statistics(root)
   return game
+
+def pit_against(config: MuZeroConfig, storage: SharedStorage, trained_network: Network):
+  player  = 1
+  num_games = 5
+  not_trained_network = storage.latest_network()
+  not_trained_network.eval()
+  trained_network.eval()
+  wins_not_trained = 0
+  wins_trainied = 0
+  draws = 0
+  for _ in range(num_games):
+    
+    game = Game(config.action_space_size, config.discount, config.environment)
+    
+    while not game.terminal():
+      cur_network = not_trained_network if player == 1 else trained_network
+      root = Node(0)
+      current_observation = game.make_image(-1)
+      expand_node(root, game.to_play(), game.legal_actions(),
+                cur_network.initial_inference(current_observation))
+      run_mcts(config, root, game.action_history(), cur_network)
+      action = select_action(config, len(game.history), root, cur_network) # review how it is salected
+      game.apply(action)
+      player *= -1
+    whowins = game.environment.getGameEnded(game.environment.board, 1)
+    if whowins == 1:
+        wins_not_trained +=1
+    elif whowins == -1:
+        wins_trainied += 1
+    else:
+        draws += 1
+  
+  return wins_not_trained, wins_trainied, draws
 
 config = make_board_game_config(16+1, 16+1, 0.25, TicTacToeEnv, 0.001)
 muzero_training(config)
