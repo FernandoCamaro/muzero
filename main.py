dba04e29ffc31f3ea338548529c71d4275bb3bfc
Fernando Camaro Nogues <fernando@ascent.ai>
2020-01-06 01:14:38 +0900

    In each episode we randomly select the oponent network from the storage. This network is only used to select the odd player actions. The search policy obtained by the mcts simulations which will be used for the training still corresponds to the current best network. More iters and more episodes per iter. In the pit function, the first move is selected with higher temperature to ensure a more diverse set of games. Learning rate is reduced to 0.005

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def muzero_training(config: MuZeroConfig):
 
   num_updates = 0
   i = 1
-  while (num_updates < 15) and (i<50):
+  while (num_updates < 50) and (i<200):
     print("ITER:",i)
     network = storage.latest_network() if num_updates > 0 else Network(config.action_space_size)
-    run_selfplay(config, network, replay_buffer, 300) # num episodes per iteration
+    run_selfplay(config, network, storage, replay_buffer, 1000, num_updates) # num episodes per iteration
     # import pickle
     # pickle.dump( replay_buffer, open( "save.pkl", "wb" ) )
     # replay_buffer = pickle.load( open( "save.pkl", "rb" ) )
@@ -44,15 +44,18 @@ def muzero_training(config: MuZeroConfig):
 
   return storage.latest_network()
 
-def run_selfplay(config: MuZeroConfig, network: Network,
-                 replay_buffer: ReplayBuffer, num_episodes: int):
+def run_selfplay(config: MuZeroConfig, network: Network, storage: SharedStorage,
+                 replay_buffer: ReplayBuffer, num_episodes: int, num_updates: int):
   network.eval()
+  
   for _ in range(num_episodes):
-    game = play_game(config, network)
+    network_op = storage.random_network() if num_updates > 0 else Network(config.action_space_size)
+    network_op.eval()
+    game = play_game(config, network, network_op)
     replay_buffer.save_game(game)
 
 
-def play_game(config: MuZeroConfig, network: Network) -> Game:
+def play_game(config: MuZeroConfig, network: Network, network_op: Network) -> Game:
   game = Game(config.action_space_size, config.discount, config.environment)
 
   while not game.terminal() and len(game.history) < config.max_moves:
@@ -67,7 +70,19 @@ def play_game(config: MuZeroConfig, network: Network) -> Game:
     # We then run a Monte Carlo Tree Search using only action sequences and the
     # model learned by the network.
     run_mcts(config, root, game.action_history(), network)
-    action = select_action(config, len(game.history), root, network)
+    
+
+    if game.to_play().id == 1:
+      action = select_action(config, len(game.history), root, network)
+    else:
+      root_op = Node(0)
+      expand_node(root_op, game.to_play(), game.legal_actions(),
+                network_op.initial_inference(current_observation))
+      add_exploration_noise(config, root_op)
+      run_mcts(config, root_op, game.action_history(), network_op)
+      action = select_action(config, len(game.history), root_op, network_op)
+      
+    
     game.apply(action)
     game.store_search_statistics(root)
   return game
@@ -83,7 +98,7 @@ def pit_against(config: MuZeroConfig, not_trained_network: Network, trained_netw
   for _ in range(num_games):
     
     game = Game(config.action_space_size, config.discount, config.environment)
-    
+    first_move = True
     while not game.terminal():
       cur_network = not_trained_network if player == 1 else trained_network
       root = Node(0)
@@ -91,7 +106,11 @@ def pit_against(config: MuZeroConfig, not_trained_network: Network, trained_netw
       expand_node(root, game.to_play(), game.legal_actions(),
                 cur_network.initial_inference(current_observation))
       run_mcts(config, root, game.action_history(), cur_network)
-      action = select_action_pit(config, len(game.history), root, cur_network, 0.2) # review how it is salected
+      if first_move:
+        action = select_action_pit(config, len(game.history), root, cur_network, 1.0) # review how it is salected
+        first_move = False
+      else:
+        action = select_action_pit(config, len(game.history), root, cur_network, 0.2) # review how it is salected
       game.apply(action)
       player *= -1
     whowins = game.environment.getGameEnded(game.environment.board, 1)
@@ -105,5 +124,5 @@ def pit_against(config: MuZeroConfig, not_trained_network: Network, trained_netw
   return wins_not_trained, wins_trainied, draws
 
 tb_logger = SummaryWriter("/tmp/tb")
-config = make_board_game_config(16+1, 16+1, 0.25, TicTacToeEnv, 0.01)
+config = make_board_game_config(16+1, 16+1, 0.25, TicTacToeEnv, 0.005)
 muzero_training(config)
