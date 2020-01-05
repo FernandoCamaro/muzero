a67edc8f4cae09f0863924071b29b2dd8bedea34
Fernando Camaro Nogues <fernando@ascent.ai>
2020-01-06 01:13:03 +0900

    use the action select function that has a temperature input argument. print the value function of the current player and state. In case of the network, print the visit count for each child, the value function computed for each child perspective during the mcts simulations, and the original prior.

--- play.py
+++ play.py
@@ -3,7 +3,7 @@ import torch
 from tictactoe.TicTacToeEnv import TicTacToeEnv
 from models.tictactoe_model import tictactoeNetwork as myNetwork
 from mcts import expand_node, run_mcts
-from util import select_action
+from util import select_action_pit
 from muzero import MuZeroConfig, make_board_game_config
 from network import Network
 from game import Game
@@ -15,7 +15,7 @@ config = make_board_game_config(16+1, 16+1, 0.25, TicTacToeEnv, 0.01)
 
 # load the trained model
 net =  myNetwork(config.action_space_size, cuda = True)
-checkpoint = torch.load("model.tar")
+checkpoint = torch.load("model_10.tar")
 net.model.load_state_dict(checkpoint["state_dict"])
 net.eval()
 
@@ -27,7 +27,10 @@ def play_game(config: MuZeroConfig, network: Network) -> Game:
   
   player = 1
   while not game.terminal():
-    
+
+    current_observation = game.make_image(-1)
+    netob  = network.initial_inference(current_observation)
+    print("value:",netob.value)
     if player == 1: # human player
         # show the board
         game.environment.display(game.environment.board)
@@ -37,14 +40,15 @@ def play_game(config: MuZeroConfig, network: Network) -> Game:
         player = -1
     else:
         root = Node(0)
-        current_observation = game.make_image(-1)
         expand_node(root, game.to_play(), game.legal_actions(),
-                    network.initial_inference(current_observation))
+                    netob)
 
         # We then run a Monte Carlo Tree Search using only action sequences and the
         # model learned by the network.
         run_mcts(config, root, game.action_history(), network)
-        action = select_action(config, len(game.history), root, network)
+        for action, child in root.children.items():
+          print(action.index, child.visit_count, child.value(), child.prior)
+        action = select_action_pit(config, len(game.history), root, network, 0.01)
         player = 1
     game.apply(action)
   whowins = game.environment.getGameEnded(game.environment.board, 1)
